65da051d85c0d86f565a20bbdba5b91ed07e80d6
Andrew <andrewyli@gmail.com>
2025-12-29 16:36:32 -0800

    trie WIP

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use atoi::atoi;
 use clap::Parser;
 use itertools::Itertools;
 use memmap2::{Mmap, MmapOptions};
-use std::collections::HashMap;
+use std::cmp::{max, min};
 use std::fs::File;
+
+use std::fmt;
+use std::fmt::{Display, Result};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -11,54 +14,149 @@ struct Args {
     input: String,
 }
 
+#[derive(Clone, Default, Debug)]
 struct Statistics {
     max: i16,
     min: i16,
-    avg: i16,
-    count: i16,
+    avg: i64,
+    count: i32,
+}
+
+impl Statistics {
+    pub fn add_val(&mut self, val: i16) {
+        self.max = max(self.max, val);
+        self.min = min(self.min, val);
+        self.avg += val as i64;
+        self.count += 1;
+    }
+}
+
+impl Display for Statistics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result {
+        write!(
+            f,
+            "{:.1}/{:.1}/{:.1}",
+            self.min as f32 * 0.1,
+            self.avg as f32 / self.count as f32 * 0.1,
+            self.max as f32 * 0.1,
+        )
+    }
+}
+
+struct StatisticsTrieNode {
+    children: [u32; 256],
+    stat: Option<Statistics>,
+    parent: u32,
+    val: u8,
+}
+
+struct StatisticsTrie {
+    nodes: Vec<StatisticsTrieNode>,
+}
+
+impl StatisticsTrie {
+    pub fn new() -> Self {
+        let mut new_trie = StatisticsTrie {
+            nodes: Vec::with_capacity(1 << 16),
+        };
+        new_trie.nodes.push(StatisticsTrieNode {
+            stat: None,
+            parent: 0,
+            children: [0; 256],
+            val: 0,
+        });
+        new_trie
+    }
+
+    #[cfg(test)]
+    pub fn get_stat(&self, arr: &[u8]) -> Option<Statistics> {
+        let mut cur_idx: u32 = 0;
+        for &b in arr {
+            let next_idx = self.nodes[cur_idx as usize].children[b as usize];
+            if next_idx == 0 {
+                return None;
+            }
+            cur_idx = next_idx;
+        }
+        self.nodes[cur_idx as usize].stat.clone()
+    }
+
+    pub fn insert(&mut self, arr: &[u8], val: i16) {
+        let mut cur_idx: u32 = 0;
+        for &b in arr {
+            cur_idx = self.get_or_create_child(cur_idx, b);
+        }
+        self.nodes[cur_idx as usize]
+            .stat
+            .get_or_insert_with(|| Statistics::default())
+            .add_val(val);
+    }
+
+    pub fn find_child(&self, parent_idx: u32, b: u8) -> u32 {
+        unsafe { self.nodes.get_unchecked(parent_idx as usize) }.children[b as usize]
+    }
+
+    pub fn get_or_create_child(&mut self, parent_idx: u32, b: u8) -> u32 {
+        let child_idx = self.find_child(parent_idx, b);
+        if child_idx > 0 {
+            return child_idx;
+        }
+        let new_node = StatisticsTrieNode {
+            stat: None,
+            parent: parent_idx,
+            children: [0; 256],
+            val: b,
+        };
+        let new_idx = self.nodes.len() as u32;
+        self.nodes.push(new_node);
+        unsafe { self.nodes.get_unchecked_mut(parent_idx as usize) }.children[b as usize] = new_idx;
+        new_idx
+    }
+
+    pub fn get_all_words_and_stats(&self) -> Vec<(String, &Statistics)> {
+        let mut pairs = Vec::new();
+        for node_idx in 0..self.nodes.len() {
+            if !self.nodes[node_idx].stat.is_none() {
+                pairs.push(self.get_word_and_stat(node_idx as u32));
+            }
+        }
+        pairs.sort_by(|a, b| a.0.cmp(&b.0));
+        pairs
+    }
+
+    pub fn get_word_and_stat(&self, leaf_idx: u32) -> (String, &Statistics) {
+        let stat = unsafe { self.nodes.get_unchecked(leaf_idx as usize) }
+            .stat
+            .as_ref()
+            .unwrap();
+        let mut word = Vec::new();
+        let mut cur_idx = leaf_idx;
+        while cur_idx != 0 {
+            word.push(unsafe { self.nodes.get_unchecked(cur_idx as usize) }.val);
+            cur_idx = unsafe { self.nodes.get_unchecked(cur_idx as usize) }.parent;
+        }
+        word.reverse();
+        (unsafe { String::from_utf8_unchecked(word) }, stat)
+    }
 }
 
 fn main() {
     let args = Args::parse();
-    let mut temp_statistics: HashMap<String, Statistics> = HashMap::new();
+    let mut temp_statistics: StatisticsTrie = StatisticsTrie::new();
     let file = File::open(args.input).unwrap();
     let mmap: Mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
     for (city_name_raw, temp_raw) in mmap.split(|&b| b == b'\n' || b == b';').tuples() {
-        let city_name: &str = unsafe { str::from_utf8_unchecked(city_name_raw) };
         let temp: i16 = atoi_times_ten(temp_raw);
-        if let Some(val) = temp_statistics.get_mut(city_name) {
-            (*val).max = std::cmp::max((*val).max, temp);
-            (*val).min = std::cmp::min((*val).min, temp);
-            (*val).avg += temp;
-            (*val).count += 1;
-        } else {
-            temp_statistics.insert(
-                city_name.to_owned(),
-                Statistics {
-                    max: temp,
-                    min: temp,
-                    avg: temp,
-                    count: 1,
-                },
-            );
-        }
+        temp_statistics.insert(city_name_raw, temp);
     }
 
-    let mut keys: Vec<_> = temp_statistics.keys().collect();
-    keys.sort();
-    let mut out_msg = "{".to_owned();
-    for key in keys {
-        let stat = temp_statistics.get(key).unwrap();
-        out_msg = format!(
-            "{}{}={:.1}/{:.1}/{:.1},",
-            out_msg,
-            key,
-            stat.min as f32 * 0.1,
-            stat.avg as f32 / stat.count as f32 * 0.1,
-            stat.max as f32 * 0.1,
-        );
-    }
-    println!("{}}}", &out_msg[..out_msg.len() - 1]);
+    let words_and_stats = temp_statistics.get_all_words_and_stats();
+    println!(
+        "{}",
+        words_and_stats.iter().format_with(", ", |(key, val), f| {
+            f(&format_args!("{}={}", key, val))
+        })
+    );
 }
 
 pub fn atoi_times_ten(s: &[u8]) -> i16 {
@@ -68,12 +166,23 @@ pub fn atoi_times_ten(s: &[u8]) -> i16 {
 #[cfg(test)]
 mod test {
 
-    use crate::atoi_times_ten;
+    use crate::{StatisticsTrie, atoi_times_ten};
 
     #[test]
     pub fn atoi_float() {
         let s = "32.8".as_bytes();
         let i: i16 = atoi_times_ten(s);
         assert_eq!(328, i);
+    }
+
+    #[test]
+    pub fn test_trie() {
+        let word = "abc".as_bytes();
+        let mut trie = StatisticsTrie::new();
+        trie.insert(word, 67);
+        assert!(trie.get_stat(b"a").is_none());
+        assert!(trie.get_stat(b"ab").is_none());
+        assert!(!trie.get_stat(b"abc").is_none());
+        println!("{}", trie.get_stat(b"abc").unwrap());
     }
 }
